b268f94684c01a6a55e5653f542d536746a44321
刘重量 <13439694341@qq.com>
2018-10-22 22:46:06 +0800

    增加root安装支持和centos7支持

--- conf/.tpl/systemctl_httpsdav.sh
+++ conf/.tpl/systemctl_httpsdav.sh
@@ -1,97 +1,155 @@
 #!/bin/bash
-# Source function library.
-HTTPSDAV_ROOT=$(readlink -f `dirname "$0"`)
-. /etc/rc.d/init.d/functions >/dev/null 2>&1
-HTTPD_LANG="C"
-# This will prevent initlog from swallowing up a pass-phrase prompt if
-# mod_ssl needs a pass-phrase from the user.
-INITLOG_ARGS=""
-OPTIONS="-C \"ServerRoot $HTTPSDAV_ROOT\" -f $HTTPSDAV_ROOT/conf/httpsdav.conf"
-# Path to the apachectl script, server binary, and short-form for messages.
-apachectl=/usr/sbin/apachectl
-if [[ ! -x $HTTPSDAV_ROOT/bin/httpsdav ]]; then
-    rm -fr $HTTPSDAV_ROOT/bin/httpsdav
-    ln -s /usr/bin/httpd $HTTPSDAV_ROOT/bin/httpsdav
-fi
-httpsdav="$HTTPSDAV_ROOT/bin/httpsdav"
-prog="httpsdav"
-pidfile=${PIDFILE-$HTTPSDAV_ROOT/run/httpsdav.pid}
-lockfile=${LOCKFILE-$HTTPSDAV_ROOT/var/subsys/httpsdav}
-RETVAL=0
-STOP_TIMEOUT=${STOP_TIMEOUT-10}
-# The semantics of these two functions differ from the way apachectl does
-# things -- attempting to start while running is a failure, and shutdown
-# when not running is also a failure.  So we just do it the way init scripts
-# are expected to behave here.
-start() {
-        echo -n $"启动 httpsdav 中: "
-        LANG=$HTTPD_LANG daemon --pidfile=${pidfile} $httpsdav $OPTIONS
-        RETVAL=$?
-        echo
-        [ $RETVAL = 0 ] && touch ${lockfile}
-        return $RETVAL
+#httpsdav 控制脚本
+#作者：刘重量 Email:13439694341@qq.com
+
+#执行前初始化环境设置
+set -e
+set -E
+OLD_LC_ALL=${LC_ALL:-""}                    #保存原来的语言编码
+export LC_ALL="zh_CN.UTF-8"                 #设置界面显示语言及编码
+
+#退出操作函数
+operate_exit() {
+    export LC_ALL="$OLD_LC_ALL"
+    exit $RETV
 }
 
-# When stopping httpd, a delay (of default 10 second) is required
-# before SIGKILLing the httpd parent; this gives enough time for the
-# httpd parent to SIGKILL any errant children.
-stop() {
-	echo -n $"关停 $prog 中: "
-	killproc -p ${pidfile} -d ${STOP_TIMEOUT} $httpd
-	RETVAL=$?
-	echo
-	[ $RETVAL = 0 ] && rm -f ${lockfile} ${pidfile}
-}
-reload() {
-    echo -n $"重载 $prog: "
-    if ! LANG=$HTTPD_LANG $httpsdav $OPTIONS -t >&/dev/null; then
-        RETVAL=6
-        echo $"not reloading due to configuration syntax error"
-        failure $"not reloading $httpsdav due to configuration syntax error"
+#获取httpsdav套件的管理目录位置
+HTTPSDAV_ROOT=$(readlink -f `dirname "$0"`)
+
+#如果目录不存在就创建
+if [ ! -d $HTTPSDAV_ROOT/bin ]; then
+    mkdir -p $HTTPSDAV_ROOT/bin
+fi
+
+httpsdav="$HTTPSDAV_ROOT/bin/httpsdav"
+
+#判断执行的前提条件
+if [ ! -x $httpsdav ]; then
+    rm -fr $httpsdav
+    if [ -f /usr/sbin/apachectl -a -x /usr/sbin/apachectl ]; then
+        ln -s /usr/sbin/apachectl $httpsdav
+    elif [ -f /usr/sbin/httpd -a -x /usr/sbin/httpd ]; then
+        ln -s /usr/sbin/httpd $httpsdav
     else
-        # Force LSB behaviour from killproc
-        LSB=1 killproc -p ${pidfile} $httpsdav -HUP
+        echo "没有在默认位置找到httpd组件，退出执行"
+        operate_exit
+    fi
+fi
+
+#基本设置
+prog="httpsdav"                                #程序名
+loglevel="error"                               #错误日志记录级别
+errorlog="$HTTPSDAV_ROOT/logs/error_log"       #错误日志的位置
+lockfile="$HTTPSDAV_ROOT/var/subsys/httpsdav"
+pidfile="$HTTPSDAV_ROOT/run/httpsdav.pid"
+
+basecommand="-d $HTTPSDAV_ROOT -f $HTTPSDAV_ROOT/conf/httpsdav.conf -e $loglevel -E $errorlog -k"
+
+RETVAL=0 #命令推出时的状态值
+
+show_success() {
+    echo -e "                   \e[33m[\e[32m OK \e[33m]\e[0m"
+    return 0
+}
+show_fail() {
+    echo -e "                   \e[33m[\e[31mFAIL\e[33m]\e[0m"
+    return 0
+}
+#启动函数
+start() {
+    echo -n "启动$prog服务:"
+    $httpsdav $basecommand start > /dev/null 2>&1
+    RETVAL=$?
+    [ $RETVAL -eq 0 ] && touch ${lockfile} && show_success && return $RETVAL
+    show_fail && return $RETVAL
+}
+
+#关闭函数
+stop() {
+    echo -n "关停$prog服务:"
+    $httpsdav $basecommand stop >& /dev/null 2>&1
+    RETVAL=$?
+    if [[ $RETVAL -eq 0 ]]; then
+        show_success
+        rm -fr ${lockfile} ${pidfile}
+    else
+        show_fail
+    fi
+    return $RETVAL
+}
+
+#重载函数
+reload() {
+    echo -n "重载$prog配置: "
+    if ( ! $httpsdav -d $HTTPSDAV_ROOT -f $HTTPSDAV_ROOT/conf/httpsdav.conf -t >& /dev/null 2>&1 ); then
+        echo -e "\n\e[31m检查配置文件中存在错误，不能重新加载配置文件\e[0m"
+        operate_exit
+    else
+        $httpsdav $basecommand graceful >& /dev/null 2>&1
         RETVAL=$?
-        if [ $RETVAL -eq 7 ]; then
-            failure $"httpsdav shutdown"
+        if [[ $RETVAL -eq 0 ]]; then
+            show_success
+        else
+            show_fail
         fi
     fi
-    echo
+    return $RETVAL
 }
 
-# See how we were called.
+#重载函数
+reload() {
+    echo -n "重载$prog配置: "
+    if ( ! $httpsdav -d $HTTPSDAV_ROOT -f $HTTPSDAV_ROOT/conf/httpsdav.conf -t >& /dev/null 2>&1 ); then
+        echo -e "\n\e[31m检查配置文件中存在错误，不能重新加载配置文件\e[0m"
+        operate_exit
+    else
+        $httpsdav $basecommand graceful >& /dev/null 2>&1
+        RETVAL=$?
+        if [[ $RETVAL -eq 0 ]]; then
+            show_success
+        else
+            show_fail
+        fi
+    fi
+    return $RETVAL
+}
+
+#重启函数
+reload() {
+    echo -n "重启$prog服务: "
+    if ( ! $httpsdav -d $HTTPSDAV_ROOT -f $HTTPSDAV_ROOT/conf/httpsdav.conf -t >& /dev/null 2>&1 ); then
+        echo -e "\n\e[31m检查配置文件中存在错误，修改正确后再重启吧？\e[0m"
+        operate_exit
+    else
+        $httpsdav $basecommand restart >& /dev/null 2>&1
+        RETVAL=$?
+        if [[ $RETVAL -eq 0 ]]; then
+            show_success
+        else
+            show_fail
+        fi
+    fi
+    return $RETVAL
+}
+
+#参数调用
 case "$1" in
-  start)
-	start
-	;;
-  stop)
-	stop
-	;;
-  status)
-        status -p ${pidfile} $httpsdav
-	RETVAL=$?
-	;;
-  restart)
-	stop
-	start
-	;;
-  condrestart|try-restart)
-	if status -p ${pidfile} $httpsdav >&/dev/null; then
-		stop
-		start
-	fi
-	;;
-  force-reload|reload)
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    restart)
+        restart
+        ;;
+    reload)
         reload
-	;;
-  graceful|help|configtest|fullstatus)
-	$apachectl $@
-	RETVAL=$?
-	;;
-  *)
-	echo $"Usage: $prog {start|stop|restart|condrestart|try-restart|force-reload|reload|status|fullstatus|graceful|help|configtest}"
-	RETVAL=2
+        ;;
+    *)
+    echo "请使用: systemctl_httpsdav.sh {start|stop|restart|reload}"
+    RETVAL=2
 esac
 
-exit $RETVAL
-
+exit $RETV
--- install.sh
+++ install.sh
@@ -12,12 +12,21 @@ export LC_ALL="zh_CN.UTF-8"                 #设置界面显示语言及编码
 HTTPSDAV_ROOT=$(readlink -f `dirname "$0"`) #获取安装目录路径
 DEFAULT_PORT=8443                           #默认启动端口号
 HOST_IP=$(hostname -I |awk '{print $1}')    #服务器IP
-USER=`whoami`
-DAV_USER="$USER"
-DAV_PATH="/home/$USER/mycloud"
+USER=`whoami`								#用户名
+DAV_USER="$USER"                            #httpsdav的运行用户
+DAV_PATH="/home/$USER/mycloud"              #httpsdav的站点目录
+USER_HOME=${HOME:-"/home/$USER"}			#安装用户主目录
+IS_INSTALLED="n"                            #是否已经安装过了且在运行；默认否
 if [ "$USER" = "root" ]; then
+    USER_HOME="/home/webdav"
     DAV_USER="webdav"
-    DAV_PATH="/home/mycloud"
+    DAV_PATH="/home/webdav/mycloud"
+fi
+APPACHE_VERSION=$(httpd -v|grep "Apache/2.4"|wc -l)
+if [ "$APPACHE_VERSION" -eq 0 ]; then
+    APPACHE_VERSION="2-2"                   #httpd版本
+else
+    APPACHE_VERSION="2-4"
 fi
 
 #安装脚本退出函数
@@ -35,17 +44,28 @@ prepare_install() {
     fi
     return 1
 }
-
+	
 #创建程序运行用户
 create_dav_user() {
     if ( ! id $DAV_USER >& /dev/null 2>&1 ); then
-        useradd -s /sbin/nologin -M $DAV_USER
+        useradd -s /bin/bash -M $DAV_USER
         DAV_GROUP="$DAV_USER"
     else
         DAV_GROUP=`id -g -n $DAV_USER`
         DAV_GROUP=${DAV_GROUP[0]}
     fi
-    chown -R $DAV_USER:$DAV_GROUP $DAV_PATH
+    chown -R $DAV_USER:$DAV_GROUP $USER_HOME
+}
+
+check_installed() {
+    IS_INSTALLED="n"
+	if ( id $DAV_USER >& /dev/null 2>&1 ); then
+		INSTALL_PATH=(`top c -u $DAV_USER -bn 1|grep "/bin/httpsdav -C ServerRoot"|awk '$15 ~/^\/home/ {print $15}'`)
+		INSTALL_PATH=${INSTALL_PATH[0]}
+	    if [[ ! -z $INSTALL_PATH  &&  -d $INSTALL_PATH ]]; then
+		    IS_INSTALLED="y"
+        fi
+	fi
 }
 
 #安装条件检查函数
@@ -77,7 +97,78 @@ httpsdav_install() {
     echo -e "\e[1m＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊\e[;36m 开始安装httpsdav \e[;0m＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊\n"
     chmod -R 700 $HTTPSDAV_ROOT    #给安装目录权限
 
-    echo -n "请输入你要启用的端口(默认：$DEFAULT_PORT)："
+    checknum=3
+    note_text="请输入位于用户目录下的webdav目录的服务器路径(值如默认：$DAV_PATH): "
+    while [ $checknum -ge 0 ]
+    do
+        read -p $note_text webdav_path
+        if [ -z $webdav_path ]; then
+            check_installed
+            if [ "$IS_INSTALLED" = "y" ]; then
+                if [ "$USER" = "root" ]; then
+                    echo "你已经为默认用户$DAV_USER安装过了,且站点在运行中，为其它用户安装吧？"
+                    continue
+                fi
+                echo "你已经安装过了，且站点在运行中"
+                USER_HOME="/home/$DAV_USER"
+                install_exit
+            fi
+            mkdir -p $DAV_PATH
+            if [ "$USER" = "root" ]; then
+                create_dav_user
+            fi
+            webdav_path="$DAV_PATH"
+            chmod 700 $webdav_path
+            break
+        fi
+        if [[ "$webdav_path" =~ ^/home/[_a-zA-Z0-9\-]+/[^/].* ]]; then
+            compare_path=${webdav_path:6}
+            user_name=${compare_path%%/*}
+			
+            if [ $user_name != "root" ]; then
+				USER_HOME="/home/$user_name"
+				if [ $USER != "root" ]; then
+					USER_HOME=${HOME:-$USER_HOME}
+				elif [ -d $webdav_path ]; then
+					path_user=`ls -ld $webdav_path|awk '{print $3}'`
+				    DAV_GROUP=`ls -ld $webdav_path|awk '{print $4}'` 
+					if [ "$path_user" != "root" ]; then
+						user_name=${path_user:-$user_name}
+						owner_home=`grep "^$user_name:" /etc/passwd|awk -F [:] '{print $6}'`
+						USER_HOME=${owner_home:-$USER_HOME}
+					fi
+				fi
+				DAV_USER=$user_name
+				check_installed
+				if [ "$IS_INSTALLED" = "y" ]; then
+					if [ "$USER" = "root" ]; then
+						note_text="你已经为该用户安装过了，且站点在运行中，请换属于其他用户目录输入:"
+						IS_INSTALLED="n"
+						continue
+					fi
+					echo "该目录用户的主目录下已经安装了webdav站点，且在运行中"
+					install_exit
+				fi
+				mkdir -p $webdav_path >& /dev/null 2>&1
+				if [ -d $webdav_path -a -r $webdav_path -a -w $webdav_path -a -x $webdav_path ]; then
+					DAV_PATH="$webdav_path"
+					chmod 700 $webdav_path
+					if [ $USER = "root" ]; then
+						create_dav_user
+					fi
+					break
+				fi
+			fi
+        fi
+        if [ $checknum -eq 0 ]; then
+           echo "多次输入错误，安装程序退出"
+           install_exit
+        fi
+        note_text="路径不支持(用户目录名不可为root)或没有rwx权限，请重新输入："
+        let "checknum--"
+    done
+	
+	echo -n "请输入你要启用的端口(默认：$DEFAULT_PORT)："
     checknum=3
     while [ $checknum -ge 0 ]
     do
@@ -93,63 +184,12 @@ httpsdav_install() {
                 note_text="端口号已被占用，请指定其它端口号输入："
             fi
         fi
-        if  [ $checknum -eq 1 ]; then
+        if  [ $checknum -eq 0 ]; then
             echo "多次输入错误，安装中止退出"
             install_exit
         fi
         let "checknum--"
         echo -n $note_text
-    done
-
-    checknum=3
-    note_text="请输入webdav目录的服务器路径(默认：$DAV_PATH): "
-    while [ $checknum -ge 0 ]
-    do
-        if [ $checknum -eq 0 ]; then
-           echo "多次输入错误，安装程序退出"
-           install_exit
-        fi
-        read -p $note_text webdav_path
-        if [ -z $webdav_path ]; then
-            mkdir -p $DAV_PATH
-            if [ "$USER" = "root" ]; then
-                create_dav_user
-            fi
-            webdav_path="$DAV_PATH"
-            chmod 700 $webdav_path
-            break
-        fi
-        note_text="输入错误或没有rwx权限，请重新输入："
-        webdav_root_path=`dirname $webdav_path`
-        if [[ "$webdav_root_path" = '.' ]]; then
-            note_text="请使用绝对路径，重新输入："
-            let "checknum--"
-            continue
-        fi
-        path_base_name=`basename $webdav_path`
-        if ( [ "$webdav_root_path" = "/" ] || ( [ "$webdav_root_path" = "/home" ] && [ "$path_base_name" = "root" ] ) ); then
-           note_text="目录不允许使用，请重新输入："
-           let "checknum--"
-           continue
-        elif [ ! -e $webdav_path -a -r $webdav_root_path -a -w $webdav_root_path -a -x $webdav_root_path ]; then
-            mkdir -p $webdav_path
-            DAV_PATH="$webdav_path"
-            chmod 700 $webdav_path
-        fi
-        if [ -d $webdav_path -a -r $webdav_path -a -w $webdav_path -a -x $webdav_path ]; then
-            if [ "$USER" != "root" ]; then
-                break
-            elif [ -s $webdav_path ]; then
-                if [ "$webdav_root_path"="/home" ]; then
-                    DAV_USER="$path_base_name"
-                fi
-                create_dav_user
-                break
-            else
-                note_text="不允许使用root用户的非空目录，请重新输入"
-            fi
-        fi
-        let "checknum--"
     done
 
     read -p "请输入你要设置的身份认证登录名(默认:$DAV_USER): " wedav_login
@@ -167,60 +207,66 @@ httpsdav_install() {
 
     echo -n "    构建httpsdav的目录结构 ----------------------------------------"
     if [ "$USER" = "root" ]; then
-        mkdir -p $HTTPSDAV_ROOT/sites/$DAV_USER
-        HTTPSDAV_ROOT="$HTTPSDAV_ROOT/sites/$DAV_USER"
+        mkdir -p $USER_HOME/httpsdav
+        mkdir -p $USER_HOME/httpsdav/conf/.tpl
+        cp -R $HTTPSDAV_ROOT/scripts $USER_HOME/httpsdav/scripts
+        cp -R $HTTPSDAV_ROOT/conf/.tpl/* $USER_HOME/httpsdav/conf/.tpl/
+        cp -R $HTTPSDAV_ROOT/conf/httpd.conf $USER_HOME/httpsdav/conf/httpd.conf
+        HTTPSDAV_ROOT="$USER_HOME/httpsdav"
     fi
-    mkdir -p $HTTPSDAV_ROOT/{bin,var,logs,run}
+    mkdir -p $HTTPSDAV_ROOT/{bin,var,logs,run,sslcache}
     mkdir -p $HTTPSDAV_ROOT/var/{subsys,dav}
-    mkdir -p $HTTPSDAV_ROOT/conf/virtualhost
-    mkdir -p $HTTPSDAV_ROOT/conf/davs/$webdav_port
+    mkdir -p $HTTPSDAV_ROOT/conf/davs
     echo -e $succ_txt
 
     echo -n "    创建用户登录身份认证文件 --------------------------------------"
-    auth_file="$HTTPSDAV_ROOT/conf/davs/$webdav_port/.$dav_name"
+    auth_file="$HTTPSDAV_ROOT/conf/davs/.$dav_name"
     if  ( ! $(htpasswd -nb $webdav_login $webdavpwd > $auth_file)>/dev/null 2>&1 ); then
         echo "${webdav_login}:$(openssl passwd -crypt $webdavpwd)" > $auth_file
     fi
     echo -e $succ_txt
 
     echo -n "    生成SSL证书 ---------------------------------------------------"
-    . $HTTPSDAV_ROOT/scripts/create_certs.sh > $HTTPSDAV_ROOT/logs/create_ssl.log 2>&1
+    . $HTTPSDAV_ROOT/scripts/create_certs.sh -u $DAV_USER > $HTTPSDAV_ROOT/logs/create_ssl.log 2>&1
     echo -e $succ_txt
 
     echo -n "    构建httpsdav的配置文件 ----------------------------------------"
+    modulesconf="$HTTPSDAV_ROOT/conf/.tpl/modules-${APPACHE_VERSION}.conf"
+    cat $modulesconf > $HTTPSDAV_ROOT/conf/modules.conf
+    cat $HTTPSDAV_ROOT/conf/.tpl/ssl.conf > $HTTPSDAV_ROOT/conf/ssl.conf
+    cat $HTTPSDAV_ROOT/conf/.tpl/client.conf > $HTTPSDAV_ROOT/conf/client.conf
     httpsdavconf="$HTTPSDAV_ROOT/conf/httpsdav.conf"
     echo "ServerName $HOSTNAME" > $httpsdavconf
     echo "Listen $webdav_port" >> $httpsdavconf
     echo "User $DAV_USER" >> $httpsdavconf
     echo "Group $DAV_GROUP" >> $httpsdavconf
     echo "Include conf/httpd.conf" >> $httpsdavconf
-
-    virtualhostconf="$HTTPSDAV_ROOT/conf/virtualhost/${webdav_port}.conf"
-    echo "<VirtualHost $HOST_IP:$webdav_port>" >> $virtualhostconf
-    cat $HTTPSDAV_ROOT/conf/.tpl/ssl.conf >> $virtualhostconf
-    echo "    Include conf/davs/$webdav_port/*.conf" >> $virtualhostconf
-    echo "</VirtualHost>" >> $virtualhostconf
+    echo "SSLSessionCache dbm:$HTTPSDAV_ROOT/sslcache/sslcache" >> $httpsdavconf
+    echo "<VirtualHost $HOST_IP:$webdav_port>" >> $httpsdavconf
+	echo "    ServerName $HOST_IP" >> $httpsdavconf
+	echo "    DocumentRoot $HTTPSDAV_ROOT/var/dav" >> $httpsdavconf
+    cat $HTTPSDAV_ROOT/conf/.tpl/httpsdav.conf >> $httpsdavconf
     echo -e $succ_txt
 
     echo -n "    创建本次安装httpsdav服务的第一个webdav站点 --------------------"
     webdav_name=${dav_name:='webdav'}
-    davconf="$HTTPSDAV_ROOT/conf/davs/$webdav_port/$dav_name.conf
+    davconf="$HTTPSDAV_ROOT/conf/davs/$dav_name.conf"
     echo "Alias /$webdav_name \"$webdav_path\"" > $davconf
     echo "<Directory \"$webdav_path\">" >> $davconf
     echo "   AuthUserFile \"conf/davs/.$dav_name\"" >> $davconf
     cat $HTTPSDAV_ROOT/conf/.tpl/webdav.conf >> $davconf
-    echo 
     httpd_path=`which httpd`
     httpd_path=${httpd_path%%:*}
     rm -fr $HTTPSDAV_ROOT/bin/httpsdav
     ln -s $httpd_path $HTTPSDAV_ROOT/bin/httpsdav
+    chown -R $DAV_USER:$DAV_GROUP $HTTPSDAV_ROOT
+    chmod -R 700 $HTTPSDAV_ROOT
     echo -e $succ_txt
 
     echo -e -n "\n启动你的webdav站点 ------------------------"
-    $HTTPSDAV_ROOT/bin/httpsdav -C "ServerRoot $HTTPSDAV_ROOT" -f $HTTPSDAV_ROOT/conf/httpsdav.conf --pidfile=$HTTPSDAV_ROOT/run/httpsdav.pid
+    su $DAV_USER -c "$HTTPSDAV_ROOT/bin/httpsdav -d $HTTPSDAV_ROOT -f $HTTPSDAV_ROOT/conf/httpsdav.conf -k start"
     echo -e "\e[5;32m 成功！\e[25m\n"
     mv $HTTPSDAV_ROOT/conf/.tpl/systemctl_httpsdav.sh $HTTPSDAV_ROOT/systemctl_httpsdav.sh
-    echo "$HTTPSDAV_ROOT" > $HTTPSDAV_ROOT/.install_lock
 
     echo -e "\n\e[1;35m＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊\e[;32m 安装完成 \e[;35m＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊\n\e[;0m"
     echo "    欢迎使用httpsdav"
@@ -229,19 +275,17 @@ httpsdav_install() {
     echo "    映射目录: $webdav_path/"
     echo "    登录名称：$webdav_login"
     echo "    验证密码：$webdavpwd"
-    echo "    你可以在当前目录下执行 ./systemctl_httpsdav.sh start|stop|reload|restart "
+    echo "    管理目录：$HTTPSDAV_ROOT"
+    echo "    你可以在管理目录下执行 ./systemctl_httpsdav.sh start|stop|reload|restart "
     echo "        ./systemctl_httpsdav.sh start|stop|reload|restart"
     echo "    的命令对本机的httpsdav服务程序进行 启动|关闭|重载|重启 的操作"
+    echo "    本软件为绿色软件，你可以在关闭站点后将管理目录移动到安装用户有权限的任意路径下重新启动，不影响站点运行"
     echo "    如果你有安装中遇到什么问题或有什么建议和需求，或者使用httpsdav中遇到了什么问题"
     echo "    可以联系我：刘重量;  Email:13439694341@qq.com"
     echo -e "\n\e[1;35m＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊ \e[;32m 祝你生活愉快 \e[;35m＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊\e[0m\n\n"
     return 0
 }
 
-if [[ -s "$HTTPSDAV_ROOT/.install_lock" ]]; then
-    echo '你已经安装过了'
-else
-    check_install_conditions
-    httpsdav_install
-fi
+check_install_conditions
+httpsdav_install
 install_exit
