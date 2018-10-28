15bfaa349218b188259ed7aee357f9861a279ab3
刘重量 <13439694341@qq.com>
2018-10-28 19:56:22 +0800

    增加对centos4.3的支持

--- install.sh
+++ install.sh
@@ -8,34 +8,46 @@ set -E
 trap 'echo "Fail unexpectedly on ${BASH_SOURCE[0]}:$LINENO!" >&2' ERR
 
 OLD_LC_ALL=${LC_ALL:-""}                    #保存原来的语言编码
+OLD_HOME="$HOME"                            #原来的用户主目录
 export LC_ALL="zh_CN.UTF-8"                 #设置界面显示语言及编码
 HTTPSDAV_ROOT=$(readlink -f `dirname "$0"`) #获取安装目录路径
 DEFAULT_PORT=8443                           #默认启动端口号
 HOST_IP=$(hostname -I |awk '{print $1}')    #服务器IP
 USER=`whoami`								#用户名
 DAV_USER="$USER"                            #httpsdav的运行用户
-DAV_PATH="/home/$USER/mycloud"              #httpsdav的站点目录
 USER_HOME=${HOME:-"/home/$USER"}			#安装用户主目录
 IS_INSTALLED="n"                            #是否已经安装过了且在运行；默认否
+
+#获取当前客户端访问所使用的服务器主机IP
+CONNECTION_IPS=($SSH_CONNECTION)
+if [[ -n ${CONNECTION_IPS[0]} && -n ${CONNECTION_IPS[2]} && ${CONNECTION_IPS[0]} != ${CONNECTION_IPS[2]} ]]; then
+    HOST_IP="${CONNECTION_IPS[2]}"
+fi
+
 if [ "$USER" = "root" ]; then
-    USER_HOME="/home/webdav"
     DAV_USER="webdav"
-    DAV_PATH="/home/webdav/mycloud"
+	HOME="/home/$DAV_USER"
+    if ( id $DAV_USER >& /dev/null 2>&1 ); then
+	    DAV_GROUP=(`id -g -n $USER`)
+        DAV_GROUP=${DAV_GROUP[0]}
+		HOME=`grep "^$DAV_USER:" /etc/passwd|awk -F [:] '{print $6}'`
+	fi
 else
-    DAV_GROUP=(`id -g -n`)
-    DAV_GROUP=${DAV_GROUP[0]}
+    DAV_USER="$USER"
+    DAV_GROUP=(`id -g -n $USER`)
+    DAV_GROUP=${DAV_GROUP[0]}	
 fi
-APPACHE_VERSION=$(httpd -v|grep "Apache/2.4"|wc -l)
-if [ "$APPACHE_VERSION" -eq 0 ]; then
-    APPACHE_VERSION="2-2"                   #httpd版本
-else
-    APPACHE_VERSION="2-4"
+USER_HOME="/home/$DAV_USER"
+if [ -d /home/users/$DAV_USER ]; then
+    USER_HOME="/home/users/$DAV_USER"	
 fi
+USER_HOME=${HOME:-$USER_HOME}
+DAV_PATH="$USER_HOME/mycloud"               #httpsdav的站点目录
 
 #安装脚本退出函数
 install_exit() {
     export LC_ALL="$OLD_LC_ALL"
-    unset $DAV_USER
+	export HOME="$OLD_HOME"
     exit 0;
 }
 
@@ -50,49 +62,54 @@ prepare_install() {
 	
 #创建程序运行用户
 create_dav_user() {
-    if ( ! id $DAV_USER >& /dev/null 2>&1 ); then
-        useradd -s /bin/bash -M $DAV_USER
-        DAV_GROUP="$DAV_USER"
-    else
-        DAV_GROUP=`id -g -n $DAV_USER`
-        DAV_GROUP=${DAV_GROUP[0]}
-    fi
-    chown -R $DAV_USER:$DAV_GROUP $USER_HOME
+    mkdir -p $USER_HOME
+    useradd -s /bin/bash -M $DAV_USER
+    DAV_GROUP=(`id -g -n $DAV_USER`)
+	DAV_GROUP=${DAV_GROUP[0]}
+    chown $DAV_USER:$DAV_GROUP $USER_HOME
+    chmod 700 $USER_HOME
+    usermod -d $USER_HOME $DAV_USER
 }
 
+#检查是否已安装过
 check_installed() {
     IS_INSTALLED="n"
-	if ( id $DAV_USER >& /dev/null 2>&1 ); then
-		INSTALL_PATH=(`top c -u $DAV_USER -bn 1|grep "/bin/httpsdav -C ServerRoot"|awk '$15 ~/^\/home/ {print $15}'`)
-		INSTALL_PATH=${INSTALL_PATH[0]}
-	    if [[ ! -z $INSTALL_PATH  &&  -d $INSTALL_PATH ]]; then
-		    IS_INSTALLED="y"
-        fi
-	fi
+    if [ -f $HTTPSDAV_ROOT/run/httpsdav.pid ]; then
+        IS_INSTALLED="y"
+    elif [ -f $USER_HOME/httpsdav/run/httpsdav.pid -a $USER = "root" ]; then
+        IS_INSTALLED="y"
+    fi
 }
 
 #安装条件检查函数
 check_install_conditions() {
-   if ( ! type httpd >/dev/null 2>&1 && ! prepare_install ); then
-       echo '没有安装httpd或缺少执行权限'
-       echo '请先用管理员账号执行'
-       echo '    yum -y install httpd mod_dav_fs mod_ssl'
-       echo '安装相关组建或给当前账号添加httpd的执行权限'
-       install_exit
-   fi
-   if [ ! -e "/etc/httpd/modules/mod_dav.so" -o ! -e "/etc/httpd/modules/mod_ssl.so" ] && ! prepare_install ; then
-       echo '没有安装apache的model_dav模块或mod_ssl模块'
-       echo '请先用管理员账号执行'
-       echo '    yum -y install mod_dav* mod_ssl'
-       echo '安装相应模块'
-       install_exit
-   fi
-   if ( ! type openssl >/dev/null 2>&1 && ! prepare_install ); then
-       echo '没有安装openssl或缺少执行权限, 请先用管理员账号执行'
-       echo '    yum -y install openssl'
-       echo '安装openssl 或者给当前账号添加openssl的执行权限'
-       install_exit
-   fi
+    if ( ! type httpd >/dev/null 2>&1 && ! prepare_install ); then
+        echo '没有安装httpd或缺少执行权限'
+        echo '请先用管理员账号执行'
+        echo '    yum -y install httpd mod_dav_fs mod_ssl'
+        echo '安装相关组建或给当前账号添加httpd的执行权限'
+        install_exit
+    fi
+    if [ ! -e "/etc/httpd/modules/mod_dav.so" -o ! -e "/etc/httpd/modules/mod_ssl.so" ] && ! prepare_install ; then
+        echo '没有安装apache的model_dav模块或mod_ssl模块'
+        echo '请先用管理员账号执行'
+        echo '    yum -y install mod_dav* mod_ssl'
+        echo '安装相应模块'
+        install_exit
+    fi
+    if ( ! type openssl >/dev/null 2>&1 && ! prepare_install ); then
+        echo '没有安装openssl或缺少执行权限, 请先用管理员账号执行'
+        echo '    yum -y install openssl'
+        echo '安装openssl 或者给当前账号添加openssl的执行权限'
+        install_exit
+    fi
+    if [ $USER != "root" ]; then
+        check_installed
+        if [ "$IS_INSTALLED" = "y" ]; then
+            echo "你已经安装过了，且站点在运行中"
+            install_exit
+        fi
+    fi
 }
 
 #安装httpsdav函数
@@ -106,68 +123,93 @@ httpsdav_install() {
     do
         read -p $note_text webdav_path
         if [ -z $webdav_path ]; then
-            check_installed
-            if [ "$IS_INSTALLED" = "y" ]; then
-                if [ "$USER" = "root" ]; then
+            if [ "$USER" = "root" ]; then
+                check_installed
+                if [ "$IS_INSTALLED" = "y" ]; then
                     echo "你已经为默认用户$DAV_USER安装过了,且站点在运行中，为其它用户安装吧？"
                     continue
-                fi
-                echo "你已经安装过了，且站点在运行中"
-                USER_HOME="/home/$DAV_USER"
-                install_exit
-            fi
-            mkdir -p $DAV_PATH
-            if [ "$USER" = "root" ]; then
-                create_dav_user
-            fi
+                elif ( ! id $DAV_USER >& /dev/null 2>&1 ); then
+                    create_dav_user
+                fi  
+                su $DAV_USER -c "mkdir -p $DAV_PATH"
+            else  
+                mkdir -p $DAV_PATH
+			fi
             webdav_path="$DAV_PATH"
             chmod 700 $webdav_path
             break
         fi
         if [[ "$webdav_path" =~ ^/home/[_a-zA-Z0-9\-]+/[^/].* ]]; then
             compare_path=${webdav_path:6}
-            user_name=${compare_path%%/*}
-			
-            if [ $user_name != "root" ]; then
-				USER_HOME="/home/$user_name"
-				if [ $USER != "root" ]; then
-					USER_HOME=${HOME:-$USER_HOME}
-				elif [ -d $webdav_path ]; then
-					path_user=`ls -ld $webdav_path|awk '{print $3}'`
-				    DAV_GROUP=`ls -ld $webdav_path|awk '{print $4}'` 
-					if [ "$path_user" != "root" ]; then
-						user_name=${path_user:-$user_name}
-						owner_home=`grep "^$user_name:" /etc/passwd|awk -F [:] '{print $6}'`
-						USER_HOME=${owner_home:-$USER_HOME}
-					fi
-				fi
-				DAV_USER=$user_name
-				check_installed
-				if [ "$IS_INSTALLED" = "y" ]; then
-					if [ "$USER" = "root" ]; then
-						note_text="你已经为该用户安装过了，且站点在运行中，请换属于其他用户目录输入:"
-						IS_INSTALLED="n"
-						continue
-					fi
-					echo "该目录用户的主目录下已经安装了webdav站点，且在运行中"
-					install_exit
-				fi
-				mkdir -p $webdav_path >& /dev/null 2>&1
-				if [ -d $webdav_path -a -r $webdav_path -a -w $webdav_path -a -x $webdav_path ]; then
-					DAV_PATH="$webdav_path"
-					chmod 700 $webdav_path
-					if [ $USER = "root" ]; then
-						create_dav_user
-					fi
-					break
-				fi
+			compare_home="/home/"
+			if [[ "$webdav_path" =~ ^/home/users/[_a-zA-Z0-9\-]+/[^/].* ]]; then
+				compare_path=${webdav_path:12}
+				compare_home="/home/users/"
 			fi
+            DAV_USER=${compare_path%%/*}
+			USER_HOME="$compare_home/$DAV_USER"
+            if [ $USER = "root" ]; then
+                if [ -d $webdav_path ]; then
+                    OWNER=`ls -ld $webdav_path|awk '{print $3}'`
+                    if [ "$OWNER" = "root" ]; then
+                        note_txt="不能使用所属用户为root的目录，请重新输入："
+                        let "checknum--"
+                    fi
+                    if ( id $OWNER >& /dev/null 2>&1 ); then
+                        DAV_USER="$OWNER"
+                        DAV_GROUP=`id -g -n $DAV_USER`
+                        DAV_GROUP=${DAV_GROUP[0]}
+                        owner_home=`grep "^$DAV_USER:" /etc/passwd|awk -F [:] '{print $6}'`
+                        USER_HOME=${owner_home:-$USER_HOME}
+                    else
+                        create_dav_user
+                    fi
+                    check_installed
+                    if [ "$IS_INSTALLED" = "y" ]; then
+                        note_text="你已经为该用户安装过了，且站点在运行中，请换属于其他用户目录输入："
+                        continue
+                    fi
+                    chmod -R 700 $webdav_path
+                    break
+                else
+                    OWNER="$DAV_USER"
+	                if [ -d $USER_HOME ]; then
+			            OWNER=`ls -ld $USER_HOME|awk '{print $3}'`
+				    fi
+                    if [[ -n "$OWNER" && "$OWNER" != "root" ]]; then
+                        if ( id $OWNER >& /dev/null 2>&1 ); then
+                            DAV_USER="$OWNER"
+                            DAV_GROUP=`id -g -n $DAV_USER`
+                            DAV_GROUP=${DAV_GROUP[0]}
+                            owner_home=`grep "^$DAV_USER:" /etc/passwd|awk -F [:] '{print $6}'`
+                            USER_HOME=${owner_home:-$USER_HOME}
+                            check_installed
+                            if [ "$IS_INSTALLED" = "y" ]; then
+                                note_text="你已经为该用户安装过了，且站点在运行中，请换属于其他用户目录输入:"
+                                continue
+                            fi
+                        else
+                            create_dav_user
+                        fi
+                        if ( su $DAV_USER -c "mkdir -p $webdav_path" >& /dev/null 2>&1 ); then
+                            DAV_PATH="$webdav_path"
+                            chmod 700 $webdav_path
+                            break
+                        fi
+				    fi
+                fi
+            else
+                [ ! -d $webdav_path ] && ( mkdir -p $webdav_path >& /dev/null 2>&1 ) && ( chmod 700 $webdav_path )
+                if [ -d $webdav_path -a -r $webdav_path -a -w $webdav_path -a -x $webdav_path ]; then
+                    break
+                fi
+            fi
         fi
         if [ $checknum -eq 0 ]; then
            echo "多次输入错误，安装程序退出"
            install_exit
         fi
-        note_text="路径不支持(用户目录名不可为root)或没有rwx权限，请重新输入："
+        note_text="路径不支持或没有rwx权限，请重新输入："
         let "checknum--"
     done
 	
@@ -234,6 +276,18 @@ httpsdav_install() {
     echo -e $succ_txt
 
     echo -n "    构建httpsdav的配置文件 ----------------------------------------"
+	APPACHE_VERSION=(`httpd -v|awk -F '.' '{print $2}'`)
+	APPACHE_VERSION=${APPACHE_VERSION[0]}
+	if [ $APPACHE_VERSION -eq 2 ]; then
+		APPACHE_VERSION="2-2"                   #httpd版本
+	elif [ $APPACHE_VERSION -eq 4 ]; then
+		APPACHE_VERSION="2-4"
+	elif [ -f /etc/httpd/conf/httpd.conf ]; then
+		modules_conf="$HTTPSDAV_ROOT/conf/.tpl/modules-x.conf"
+		cat /etc/httpd/conf/httpd.conf|sed -n '/LoadModule /p' > $modules_conf
+		sed -i 's/modules\/\/etc\/httpd\/modules\//g' $modules_conf
+		APPACHE_VERSION="x"
+	fi
     modulesconf="$HTTPSDAV_ROOT/conf/.tpl/modules-${APPACHE_VERSION}.conf"
     cat $modulesconf > $HTTPSDAV_ROOT/conf/modules.conf
     echo "SSLEngine Off" > $HTTPSDAV_ROOT/conf/ssl.conf
@@ -297,7 +351,7 @@ httpsdav_install() {
     echo "    开启后，如果你要关闭ssl功能，重新回到使用http，那么只要执行"
     echo "        ./my_webdav stop ssl"
     echo "    就可以了，同时你的站点访问地址也回到了原来的"
-    echo "    你还可以在你的站点目录下使用"
+    echo "    你还可以在你的管理目录下使用"
     echo "        ./my_webdav start|stop|reload|restart"
     echo "    的命令对本机的httpsdav服务程序进行 启动|关闭|重载|重启 的操作"
     echo "    本软件为绿色软件，你可以在关闭站点后将管理目录移动到安装用户主目录下的任意路径下重新启动，不影响站点运行"
